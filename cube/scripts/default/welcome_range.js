20c90e3fe31d0dc2bdb7743f8dd0db2db2aea322
Moy Wu <immoywu@gmail.com>
2021-04-03 17:10:46 +0800

    no message

--- cube/scripts/default/welcome_range.js
+++ cube/scripts/default/welcome_range.js
@@ -23,30 +23,6 @@ confidence_slider.noUiSlider.on('update', function (values) {
 });
 
 
-var totalPrice_slider = document.getElementById('totalPrice_range');
-var totalPrice_Value = document.getElementById('totalPrice_amount');
-// Total Price slider
-noUiSlider.create(totalPrice_slider, {
-    start: [500, 1500],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 3000
-    },
-    format: {
-      from: function(value) {
-              return parseInt(value);
-          },
-      to: function(value) {
-              return parseInt(value);
-      }
-    }
-});
-totalPrice_slider.noUiSlider.on('update', function (values) {
-  totalPrice_Value.innerHTML = values.join(' - ');
-});
-
-
 var confidence_slider2 = document.getElementById('confidence_range2');
 var confidence_Value2 = document.getElementById('confidence_amount2');
 
@@ -69,28 +45,4 @@ noUiSlider.create(confidence_slider2, {
 });
 confidence_slider2.noUiSlider.on('update', function (values) {
   confidence_Value2.innerHTML = values.join(' - ');
-});
-
-
-var totalPrice_slider2 = document.getElementById('totalPrice_range2');
-var totalPrice_Value2 = document.getElementById('totalPrice_amount2');
-// Total Price slider
-noUiSlider.create(totalPrice_slider2, {
-    start: [500, 1500],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 3000
-    },
-    format: {
-      from: function(value) {
-              return parseInt(value);
-          },
-      to: function(value) {
-              return parseInt(value);
-      }
-    }
-});
-totalPrice_slider2.noUiSlider.on('update', function (values) {
-  totalPrice_Value2.innerHTML = values.join(' - ');
 });
